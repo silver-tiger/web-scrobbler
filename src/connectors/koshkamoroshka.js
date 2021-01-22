adfd5f720263af1a32146729fb0a3cd4f96c9d1c
Ilya Safronov <ie@safronov.info>
2021-01-22 07:50:04 +0300

    Create koshkamoroshka.js

---
+++ src/connectors/koshkamoroshka.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Connector.playerSelector = '.playlist';
+
+Connector.artistSelector = '.player-artist-text';
+
+Connector.trackSelector = '.title';
+
+Connector.playButtonSelector = '.fa-play';
+
+Connector.onReady = Connector.onStateChanged;
